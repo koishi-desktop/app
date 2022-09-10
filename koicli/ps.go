93d76cbdb01a693971dcb385d65fcc12f32841f0
Il Harper <hi@ilharper.com>
2022-09-10 19:31:00 +0000

    chore: publish koishijs/koishi-desktop@e6874d152eb3a77acfba11cb0729a2264c2ae656

--- koicli/ps.go
+++ koicli/ps.go
@@ -1,14 +1,15 @@
-//nolint:wrapcheck
 package koicli
 
 import (
 	"errors"
 	"fmt"
 
+	"github.com/goccy/go-json"
 	"github.com/mitchellh/mapstructure"
 	"github.com/samber/do"
 	"github.com/urfave/cli/v2"
 	"gopkg.ilharper.com/koi/core/god/proto"
+	"gopkg.ilharper.com/koi/core/koicmd"
 	"gopkg.ilharper.com/koi/core/koiconfig"
 	"gopkg.ilharper.com/koi/core/logger"
 	"gopkg.ilharper.com/koi/sdk/client"
@@ -16,41 +17,35 @@ import (
 )
 
 const (
-	serviceCommandImport = "gopkg.ilharper.com/koi/app/koicli/command.Import"
-	serviceActionImport  = "gopkg.ilharper.com/koi/app/koicli/action.Import"
+	serviceCommandPs = "gopkg.ilharper.com/koi/app/koicli/command.Ps"
+	serviceActionPs  = "gopkg.ilharper.com/koi/app/koicli/action.Ps"
 )
 
-func newImportCommand(i *do.Injector) (*cli.Command, error) {
-	do.ProvideNamed(i, serviceActionImport, newImportAction)
+func newPsCommand(i *do.Injector) (*cli.Command, error) {
+	do.ProvideNamed(i, serviceActionPs, newPsAction)
 
 	return &cli.Command{
-		Name:      "import",
-		Usage:     "Import a Koishi Bundle",
-		ArgsUsage: "path",
-		Action:    do.MustInvokeNamed[cli.ActionFunc](i, serviceActionImport),
+		Name:   "ps",
+		Usage:  "Show Process Status",
+		Action: do.MustInvokeNamed[cli.ActionFunc](i, serviceActionPs),
 
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "name",
-				Aliases: []string{"n"},
-				Usage:   "Name of the imported instance",
-			},
-
 			&cli.BoolFlag{
-				Name:  "force",
-				Usage: "Empty instance directory before creating",
+				Name:    "all",
+				Aliases: []string{"a"},
+				Usage:   "Including stopped instances",
 			},
 		},
 	}, nil
 }
 
-func newImportAction(i *do.Injector) (cli.ActionFunc, error) {
+func newPsAction(i *do.Injector) (cli.ActionFunc, error) {
 	l := do.MustInvoke[*logger.Logger](i)
 
 	return func(c *cli.Context) error {
 		var err error
 
-		l.Debug("Trigger action: import")
+		l.Debug("Trigger action: ps")
 
 		cfg, err := do.Invoke[*koiconfig.Config](i)
 		if err != nil {
@@ -63,11 +58,9 @@ func newImportAction(i *do.Injector) (cli.ActionFunc, error) {
 			return err
 		}
 
-		respC, logC, err := client.Import(
+		respC, logC, err := client.Ps(
 			conn,
-			c.Args().First(),
-			c.String("name"),
-			c.Bool("force"),
+			c.Bool("all"),
 		)
 		if err != nil {
 			return err
@@ -100,6 +93,19 @@ func newImportAction(i *do.Injector) (cli.ActionFunc, error) {
 
 			return errors.New(s)
 		}
+
+		var resultPs koicmd.ResultPs
+		err = mapstructure.Decode(result.Data, &resultPs)
+		if err != nil {
+			return fmt.Errorf("failed to parse result %#+v: %w", result, err)
+		}
+
+		resultPsInstanceJson, err := json.Marshal(resultPs)
+		if err != nil {
+			return fmt.Errorf("failed to marshal result %#+v: %w", resultPs, err)
+		}
+
+		fmt.Println(string(resultPsInstanceJson))
 
 		return logger.Wait(respC)
 	}, nil
--- ui/tray/tray.go
+++ ui/tray/tray.go
@@ -2,14 +2,26 @@ package tray
 
 import (
 	"runtime"
+	"time"
 
 	"fyne.io/systray"
+	"github.com/mitchellh/mapstructure"
 	"github.com/samber/do"
-	"gopkg.ilharper.com/koi/app/ui/icon"
+	"gopkg.ilharper.com/koi/app/util"
+	"gopkg.ilharper.com/koi/core/god/proto"
+	"gopkg.ilharper.com/koi/core/koicmd"
+	"gopkg.ilharper.com/koi/core/koiconfig"
 	"gopkg.ilharper.com/koi/core/logger"
+	"gopkg.ilharper.com/koi/core/ui/icon"
+	"gopkg.ilharper.com/koi/sdk/client"
+	"gopkg.ilharper.com/koi/sdk/manage"
 )
 
+const refreshDuration = 3 * time.Second
+
 func Run(i *do.Injector) error {
+	do.ProvideNamed(i, serviceTrayChannelRegistry, NewChannelRegistry)
+
 	systray.Run(buildOnReady(i), nil)
 
 	return nil
@@ -19,22 +31,159 @@ func buildOnReady(i *do.Injector) func() {
 	l := do.MustInvoke[*logger.Logger](i)
 
 	return func() {
+		var err error
+
+		l.Debug("Tray ready.")
+
 		if runtime.GOOS != "darwin" {
 			systray.SetTitle("Koishi")
 		}
-		systray.SetTooltip("Koishi")
+		if runtime.GOOS != "windows" {
+			systray.SetTooltip("Koishi")
+		}
 		systray.SetTemplateIcon(icon.Data, icon.Data)
 
-		mQuit := systray.AddMenuItem("Hide", "Hide Tray Button")
+		mStarting := systray.AddMenuItem("Starting...", "Starting...")
+		mStarting.Disable()
+		addHide(i)
 
-		go func() {
-			for {
-				select {
-				case <-mQuit.ClickedCh:
-					l.Debugf("Exiting systray")
-					systray.Quit()
-				}
-			}
-		}()
+		cfg, err := do.Invoke[*koiconfig.Config](i)
+		if err != nil {
+			l.Error(err)
+			systray.Quit()
+		}
+
+		manager := manage.NewKoiManager(cfg.Computed.Exe, cfg.Computed.DirLock)
+		go trayDaemon(i, manager)
 	}
+}
+
+func trayDaemon(i *do.Injector, manager *manage.KoiManager) {
+	l := do.MustInvoke[*logger.Logger](i)
+	channelRegistry := do.MustInvokeNamed[*ChannelRegistry](i, serviceTrayChannelRegistry)
+
+	for {
+		var err error
+
+		conn, err := manager.Ensure()
+		if err != nil {
+			l.Error(err)
+
+			continue
+		}
+
+		respC, logC, err := client.Ps(conn, true)
+		if err != nil {
+			l.Error(err)
+
+			continue
+		}
+
+		logger.LogChannel(i, logC)
+
+		var result proto.Result
+		response := <-respC
+		if response == nil {
+			l.Error("failed to get result, response is nil")
+
+			continue
+		}
+
+		if response.Type != proto.TypeResponseResult {
+			l.Errorf("failed to parse result %#+v: response type '%s' is not '%s': %v", response, response.Type, proto.TypeResponseResult, err)
+
+			continue
+		}
+
+		err = mapstructure.Decode(response.Data, &result)
+		if err != nil {
+			l.Errorf("failed to parse result %#+v: %v", response, err)
+
+			continue
+		}
+
+		if result.Code != 0 {
+			s, ok := result.Data.(string)
+			if !ok {
+				l.Errorf("result data %#+v is not string", result.Data)
+
+				continue
+			}
+			l.Error(s)
+
+			continue
+		}
+
+		var resultPs koicmd.ResultPs
+		err = mapstructure.Decode(result.Data, &resultPs)
+		if err != nil {
+			l.Errorf("failed to parse result %#+v: %w", result, err)
+
+			continue
+		}
+
+		instances := resultPs.Instances
+
+		// Clear all menu items.
+		systray.ResetMenu()
+
+		addInfo(i)
+
+		// Iterate all instances.
+		for _, instance := range instances {
+			// Add menu items for each instance.
+			m := systray.AddMenuItem(instance.Name, instance.Name)
+			mOpen := m.AddSubMenuItem("Open", "Open")
+			mStart := m.AddSubMenuItem("Start", "Start")
+			mRestart := m.AddSubMenuItem("Restart", "Restart")
+			mStop := m.AddSubMenuItem("Stop", "Stop")
+			if instance.Running {
+				mStart.Disable()
+			} else {
+				mRestart.Disable()
+				mStop.Disable()
+			}
+
+			channelRegistry.Insert(mOpen.ClickedCh)
+			channelRegistry.Insert(mStart.ClickedCh)
+			channelRegistry.Insert(mRestart.ClickedCh)
+			channelRegistry.Insert(mStop.ClickedCh)
+		}
+
+		addHide(i)
+
+		<-time.NewTimer(refreshDuration).C
+	}
+}
+
+func addInfo(i *do.Injector) {
+	channelRegistry := do.MustInvokeNamed[*ChannelRegistry](i, serviceTrayChannelRegistry)
+
+	mTitle := systray.AddMenuItem("Koishi Desktop", "Koishi Desktop")
+	mTitle.Disable()
+	version := "v" + util.AppVersion
+	mVersion := systray.AddMenuItem(version, version)
+	mVersion.Disable()
+	systray.AddSeparator()
+
+	channelRegistry.Insert(mTitle.ClickedCh)
+	channelRegistry.Insert(mVersion.ClickedCh)
+}
+
+func addHide(i *do.Injector) {
+	l := do.MustInvoke[*logger.Logger](i)
+	channelRegistry := do.MustInvokeNamed[*ChannelRegistry](i, serviceTrayChannelRegistry)
+
+	systray.AddSeparator()
+	mQuit := systray.AddMenuItem("Hide", "Hide Tray Button")
+
+	channelRegistry.Insert(mQuit.ClickedCh)
+
+	go func() {
+		_, ok := <-mQuit.ClickedCh
+		if ok {
+			l.Debugf("Exiting systray")
+			systray.Quit()
+		}
+	}()
 }
