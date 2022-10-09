c9469eb918ae9f0717af638c4ef358004198df2d
Il Harper <hi@ilharper.com>
2022-10-09 16:30:27 +0000

    chore: publish koishijs/koishi-desktop@bf20231f23813a277b2637341ee056b0abd57314

--- koicli/pre.go
+++ koicli/pre.go
@@ -3,10 +3,13 @@ package koicli
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/samber/do"
 	"github.com/urfave/cli/v2"
 	"gopkg.ilharper.com/koi/app/config"
+	"gopkg.ilharper.com/koi/core/koishell"
 	"gopkg.ilharper.com/koi/core/logger"
 	"gopkg.ilharper.com/x/rpl"
 )
@@ -37,6 +40,14 @@ func newPreAction(i *do.Injector) (cli.BeforeFunc, error) {
 		l.Debugf("Command line arguments:\n%#+v", os.Args)
 
 		do.Provide(i, config.BuildLoadConfig("koi.yml"))
+
+		var shellName string
+		if runtime.GOOS == "windows" {
+			shellName = "koishell.exe"
+		} else {
+			shellName = "koishell"
+		}
+		do.Provide(i, koishell.BuildKoiShell(filepath.Join(filepath.Dir(exe), shellName)))
 
 		return nil
 	}, nil
